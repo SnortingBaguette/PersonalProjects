4eb0c54870978e419b2f5aa7eeca161b725e4692
Dmitry Panchuk <10994198@uvu.edu>
2024-05-23 01:15:09 -0600

    Made the cursor position adjust to the resolution

--- Coding/MagickaRipOff/Assets/Scripts/CameraMovement.cs
+++ Coding/MagickaRipOff/Assets/Scripts/CameraMovement.cs
@@ -10,22 +10,31 @@ public class CameraMovement : MonoBehaviour
     public float copiedPositionZ;   //Create a variable to store the player's Z position
     public Vector3 positionForTheCamera;    //Create a Vector that will store the new camera position
     public Vector3 cursorCoordinates;   //A vector to store the mouse coordinates on a 1080p screen
+    int screenHeight;
+    int screenHeightHalf;
+    int screenWidth;
+    int screenWidthHalf;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        screenHeight = Screen.height / 2;
+
+        screenWidth = Screen.width / 2;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        cursorCoordinates.x = (Input.mousePosition.x - 960) / 960;  //Clamp the cursor coordinates to 1
-        cursorCoordinates.y = (Input.mousePosition.y - 590) / 590;  //Clamp the cursor coordinates to 1
+        cursorCoordinates.x = (Input.mousePosition.x - screenWidth) / screenWidth;  //Clamp the cursor coordinates to 1
+        cursorCoordinates.y = (Input.mousePosition.y - screenHeight) / screenHeight;  //Clamp the cursor coordinates to 1
         copiedPositionX = playerObject.transform.position.x;    //Copy the position of the player in order to reference it
         copiedPositionZ = playerObject.transform.position.z;    //Copy the position of the player in order to reference it
         positionForTheCamera.Set(copiedPositionX + cursorCoordinates.x, 9.45f, copiedPositionZ - 6.25f + cursorCoordinates.y);  //Math for making the camera follow the player only on x and z axis
         transform.position = positionForTheCamera;  //Setting the math result as the camera position
+        Debug.Log(cursorCoordinates.y);
     }
 }
