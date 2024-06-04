f136df49c71312fd8bc149b0ade89f0635861e88
Dmitry Panchuk <10994198@uvu.edu>
2024-06-04 00:26:59 -0600

    WIP for the Magicka 2 movement style

--- Coding/MagickaRipOff/Assets/Scripts/PlayerMovement.cs
+++ Coding/MagickaRipOff/Assets/Scripts/PlayerMovement.cs
@@ -10,16 +10,23 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection;
 
 
+
     public Transform lookAtTarget;
     private Vector3 lookDirection;
     private Quaternion rotation;
     public float rotationSpeed = 12f;
+
+    public GameObject moveAfterMouseLetGo;
+    private Vector3 movementFinalTarget;
+    bool shouldStop;
+    private Vector3 directionToSavesPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        movementFinalTarget  = moveAfterMouseLetGo.transform.position;
     }
 
     // Update is called once per frame
@@ -29,13 +36,36 @@ public class PlayerMovement : MonoBehaviour
         lookDirection.z = lookAtTarget.position.z - transform.position.z;
         rotation = Quaternion.LookRotation(lookDirection);
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+
+        /*movementFinalTarget = moveAfterMouseLetGo.transform.position;
+        directionToSavesPos.x = movementFinalTarget.x - transform.position.x;
+        directionToSavesPos.z = movementFinalTarget.z - transform.position.x;
+        moveDirection = Vector3.Normalize(directionToSavesPos); */
+
         moveDirection = Vector3.Normalize(lookDirection);
+
+
         //Vector3.Normalize(lookDirection);
+
+        /*if (Vector3.Distance(movementFinalTarget, transform.position) <= 1)
+        {
+            Debug.Log("The character will stop now");
+            shouldStop = true;
+
+        } else
+        {
+            Debug.Log("No stopping");
+            shouldStop = false;
+        }   */
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
             characterController.Move(moveDirection * characterSpeed * Time.deltaTime);
         }
+
+        //Debug.Log(movementFinalTarget);
+        //Debug.Log(Vector3.Distance(movementFinalTarget, transform.position));
     }
 
 
